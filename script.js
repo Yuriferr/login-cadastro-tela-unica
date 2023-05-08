e827f6283a19d1ba0c8218983a701ebf5c7bb131
Yuriferr <yurifernandespreto@gmail.com>
2023-05-08 15:19:54 -0300

    Final mobile version

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 const parede = document.getElementById("parede")
 const imgParede = document.getElementById("imgParede")
+
+var screen = screen.width
  
 
 function moverParedeCadastro(imgCadastro){
-    parede.style.marginLeft = "-30%";
-    imgParede.src = imgCadastro;
+    if(screen > 700){
+        parede.style.marginRight = '50%';
+        imgParede.src = imgCadastro;
+    }else{
+        parede.style.marginBottom = '275px';
+        imgParede.src = imgCadastro;
+    }
 }
 
 function moverParedeLogin(imgLogin){
-    parede.style.marginLeft = "30%";
-    imgParede.src = imgLogin;
+    if(screen > 700){
+        parede.style.marginRight = '0';
+        imgParede.src = imgLogin;
+    }else{
+        parede.style.marginBottom = '0';
+        imgParede.src = imgLogin;
+    }
 }
